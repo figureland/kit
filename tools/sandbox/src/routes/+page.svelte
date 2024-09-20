0a159d63fdf701c70962a2ec640debde476ba940
Toby Milner-Gulland <toby@figure.land>
2024-09-20 17:51:14 +0100

    chore: minor updates to decimal helper

--- tools/sandbox/src/routes/+page.svelte
+++ tools/sandbox/src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
   import { wrap, state } from '@figureland/kit/state/svelte'
-  import { decimal } from '@figureland/kit/decimal'
+  import { decimal } from '@figureland/kit/math/decimal'
   const s = state(0.000001)
   const w = wrap(decimal(0.000001))
 </script>
